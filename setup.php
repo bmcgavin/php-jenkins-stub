a874dd567a0a3f0ed4aa75cc27b5ea91db004f4b
Richard Jones <rich@linuxplicable.org>
2012-03-05 21:23:54 +0000

    init script

---
+++ setup.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+
+if (!array_key_exists(1, $argv)) {
+	die('Need new name' . PHP_EOL);
+}
+
+$old = 'changeme';
+$uold = 'CHANGEME';
+
+$new = strtolower($argv[1]);
+$unew = strtoupper($new);
+
+$files = array(
+	"features/" . $uold . ".feature",
+	"features/step_definitions/" . $uold . "_steps.php",
+	"src/" . $uold . ".php",
+	"tests/unit/" . $uold . "Test.php"
+);
+
+$contents = array(
+	"phpdox.xml.dist",	
+	"phpunit.xml.dist",
+	"src/" . $unew . ".php",
+	"tests/unit/" . $unew . "Test.php"
+);
+
+foreach($files as $file) {
+	$new = str_replace($uold, $unew, $file);
+	rename($file, $new);
+}
+
+foreach($contents as $file) {
+	$data = file_get_contents($file);
+	$data = preg_replace("/" . $uold . "/", $unew, $data);
+	$data = preg_replace("/" . $old . "/", $new, $data);
+	file_put_contents($file, $data);
+}
+
+
